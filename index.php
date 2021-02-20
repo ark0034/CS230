a19e19a0dd217832f825f6f8d6b4a463f62e1fb9
ark0034 <ark0034@mix.wvu.edu>
2021-02-19 20:54:21 -0500

    Lab2_aboutpage

---
+++ index.php
@@ -0,0 +1,42 @@
+<main>
+    <link rel= "stylesheet" href= "css/about.css">
+
+    <div class="about">
+        <h1>About us</h1>
+        <p>We are wholesome and love crochet</p>
+    <div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/first.jpeg">
+                <div class="container">
+                    <h2>Founder</h2>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/second.jpeg">
+                <div class="container">
+                    <h2>Website Management</h2>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/third.jpeg">
+                <div class="container">
+                    <h2>Technical Support</h2>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/fourth.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
